795108c50b409a1f4bc0730e9466d0d4336069c0
chriskimty <christaeyoung.kim@gmail.com>
2022-12-04 02:34:25 -0500

    finished lesson 7

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -12,19 +13,12 @@ const app = Vue.createApp({
             ]
         }
     },
-    // can add methods right into here
-        methods: {
-            addToCart() {
-                this.cart += 1
-                // this refers to the cart in the data
-            },
-            removeFromCart() {
-                if (this.cart > 0) {
-                    this.cart--
-                }
-            },
-            updateImage(variantImage) {
-                this.image = variantImage
-            }
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
+    }
 })
