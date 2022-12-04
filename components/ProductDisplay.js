8d825ea49a9f4c469f77e2b0ab2aed621b1dc568
chriskimty <christaeyoung.kim@gmail.com>
2022-12-04 12:54:52 -0500

    completed lesson 10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,76 +1,100 @@
-// first argument is name, and object to configure our component
 app.component('product-display', {
-    props: {
-        // props from main.js
-        premium: {
-            // Prop validation
-            type: Boolean,
-            required: true
-        }
-    },
-    template: 
-        // this syntax below allows for syntax highlighting (with the es6 html string extension we installed)
-    /*html*/
-    `<div class="product-display">
-      <div class="product-container">
-        <div class="product-image">
-          <img v-bind:src="image">
-        </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-
-          <p>Shipping: {{ shipping }}</p>
-          
-          <product-details :details="details"></product-details>
-
-          <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
-            class="color-circle" :style="{ backgroundColor: variant.color }">
-          </div>
-
-          <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add
-            to Cart</button>
-        </div>
-      </div>
-    </div>`,
-        data() {
-        return {
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        shipping() {
-            if (this.premium) {
-                return 'Free'
-            } {
-            return '$2.99'
-            }
-        }
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
     }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
+        </div>
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
+      </div>
+    </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
+  </div>`,
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+        ],
+        reviews: []
+    }
+  },
+  methods: {
+      addToCart() {
+        //   *emit - bubbling up that event 
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+        //   ^this id is the payload
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
+      }
+  }
 })
--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            // setting cart as array that has id of the product we pushed into it (this is more realistic)
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItems(id) {
+            // my solution
+            this.cart.pop(id)
+            // suggested solution: 
+            //   const index = this.cart.indexOf(id)
+            //     if (index > -1) {
+            //         this.cart.splice(index, 1)
+            //     }
+        }
+    }
 })
